444eaf5b7ceda92a4a92c9ebc879966230ab698b
Mariusz Winnik <tazo90@gmail.com>
2025-12-15 21:17:58 +0100

    fix: disable SSR for swagger-ui-react in latest version (#68)

--- src/components/swagger.ts
+++ src/components/swagger.ts
@@ -8,7 +8,6 @@ import "swagger-ui-react/swagger-ui.css";
 import dynamic from "next/dynamic";
 
 const SwaggerUI = dynamic(() => import("swagger-ui-react"), {
-  ssr: false,
   loading: () => <p>Loading Component...</p>,
 });
 
