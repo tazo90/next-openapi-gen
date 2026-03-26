f8baab509d5e44802d0d2075d54b6e5b8e7c1247
Martijn van Dijk <mhvdijk@gmail.com>
2026-03-26 21:41:06 +0100

    chore: move to ts

--- examples/next15-app-drizzle-zod/tailwind.config.ts
+++ examples/next15-app-drizzle-zod/tailwind.config.ts
@@ -1,5 +1,6 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from "tailwindcss";
+
+const config = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,4 +10,6 @@ module.exports = {
     extend: {},
   },
   plugins: [],
-};
+} satisfies Config;
+
+export default config;
